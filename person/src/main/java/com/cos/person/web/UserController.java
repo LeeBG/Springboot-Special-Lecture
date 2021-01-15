38555abbe383215dbf25314d4950ad547328ac96
LeeBG <donny1848@gmail.com>
2021-01-15 17:48:57 +0900

    AOP 실습 Youtube보면서 복습할 것

--- person/src/main/java/com/cos/person/web/UserController.java
+++ person/src/main/java/com/cos/person/web/UserController.java
@@ -2,7 +2,11 @@ package com.cos.person.web;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -48,7 +52,8 @@ public class UserController {
 	// x-www-form-urlencoded => request.getParamter()
 	// text/plain => @RequestBody 어노테이션
 	// application/json => @ResponseBody 어노테이션 + 오브젝트로 받기
-	public CommonDto<String> save(@RequestBody JoinReqDto dto) {
+	public CommonDto<String> save(@Valid @RequestBody JoinReqDto dto,BindingResult bindingResult) {
+		
 		System.out.println("save()");
 		System.out.println("user : "+dto);
 		userRepository.save(dto);
@@ -71,7 +76,8 @@ public class UserController {
 	
 	// http://localhost:8080/user/2
 	@PutMapping("/user/{id}")
-	public CommonDto update(@PathVariable int id, @RequestBody UpdateReqDto dto) {
+	public CommonDto update(@PathVariable int id,@Valid  @RequestBody UpdateReqDto dto, BindingResult bindingResult) {
+		
 		System.out.println("update()");	
 		userRepository.update(id, dto);
 		return new CommonDto<>(HttpStatus.OK.value()); 
